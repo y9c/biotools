13f5820e06a7ea5fdb56e397de193b7e327a66c5
yech1990 <yech1990@gmail.com>
2016-05-19 15:09:02 +0800

    update

---
+++ newick-1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name="newick",
+      version="1.3",
+      description="A parser for the Newick tree format.",
+      long_description="A parser for the Newick tree format.",
+
+      url="http://www.daimi.au.dk/~mailund/newick.html",
+      author="Thomas Mailund",     author_email="mailund@birc.au.dk",
+      maintainer="Thomas Mailund", maintainer_email="mailund@birc.au.dk",
+      contact="Thomas Mailund",    contact_email="mailund@birc.au.dk",
+
+      license="General Public License (GPL)",
+      packages=["newick"])
